d53a71d38000044754f0664e22229c40cb6905ae
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-09-08 00:36:45 +0530

    server_added

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const app=express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 app.use("/products",productRouter)
 app.get("/", (req, res) => {
     res.send("School Management System API");
